38cc3ccab105339632aefefe48c96ce8126efc1f
Kavinu Thepul <kavinuthepul456@gmail.com>
2026-03-08 20:51:13 +0530

    feat: update base path in vite.config.ts for production mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/portfolio/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Portfolio/' : '/',
   plugins: [react()],
-})
+}))
