cd99c3958b5898becf0c427b8e38e7750c5a3493
Kavinu Thepul <kavinuthepul456@gmail.com>
2026-03-19 19:47:11 +0530

    refactor: configure Vite base path using the `VITE_BASE_PATH` environment variable.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === "build" ? "/Portfolio/" : "/",
-}));
+  base: process.env.VITE_BASE_PATH || "/",
+});
